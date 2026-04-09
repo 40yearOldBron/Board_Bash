2d600ba1ecc428822b35c560917b5862fd010f48
JackTheCoder303 <JTDubs0909@gmail.com>
2026-04-09 08:41:21 -0500

    V2

--- Board_Bash/ContentView.swift
+++ Board_Bash/ContentView.swift
@@ -9,13 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            VStack{
+                HStack{Button("Settings") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                    
+                .frame(width:  150, height:100, alignment: .center)
+                .background(Color.yellow)
+                .padding(.trailing)
+                .padding(.bottom,800.0)
+                    Spacer()
+                }
+            }
+            VStack{
+              
+                HStack{
+                   
+                    Button("Ranks") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width:  140, height:70, alignment: .center)
+                    .background(Color.yellow)
+                    .padding(.leading, 75.0)
+                    .padding(.trailing, 25.0)
+                    
+                    Button("Friends") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                      
+                    }
+                    .frame(width:  140, height:70, alignment: .center)
+                    .background(Color.yellow)
+                    .padding(.trailing, 75.0)
+                }
+                .padding(.bottom, 450.0)
+            }
+            VStack{
+                Button("Shop") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+                .padding(.bottom, 100.0)
+                Button("Play") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                
+            }
+                
+            
         }
-        .padding()
     }
 }
 
