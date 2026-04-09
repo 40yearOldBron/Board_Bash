abbfc02705f704bbf6c0569565b3003d6069fda7
JackTheCoder303 <JTDubs0909@gmail.com>
2026-04-09 09:12:45 -0500

    Buttons V1

--- Board_Bash/ContentView.swift
+++ Board_Bash/ContentView.swift
@@ -16,11 +16,13 @@ struct ContentView: View {
                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                 }
                     
-                .frame(width:  150, height:100, alignment: .center)
+                .frame(width:  180, height:100, alignment: .center)
                 .background(Color.yellow)
                 .padding(.trailing)
                 .padding(.bottom,800.0)
+                .font(.largeTitle)
                     Spacer()
+                        
                 }
             }
             VStack{
@@ -28,30 +30,43 @@ struct ContentView: View {
                     Button("Ranks") {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     }
-                    .frame(width:  140, height:70, alignment: .center)
+                    .frame(width:  190, height:70, alignment: .center)
                     .background(Color.yellow)
                     .padding(.leading, 75.0)
                     .padding(.trailing, 25.0)
-                    
+                    .font(.largeTitle)
+          
                     Button("Friends") {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                       
                     }
-                    .frame(width:  140, height:70, alignment: .center)
+ 
+                    .frame(width:  190, height:70, alignment: .center)
                     .background(Color.yellow)
                     .padding(.trailing, 75.0)
+                    .font(.largeTitle)
                 }
-                .padding(.bottom, 450.0)
+                .offset(x:0, y:-205.0)
             }
             .padding(2.0)
             VStack{
                 Button("Shop") {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
             }
-                .padding(.bottom, 100.0)
+                .frame(width:  300, height: 100, alignment: .center)
+                .background(Color.yellow)
+                
+                .offset(x:5.0, y:67.0)
+                .font(.largeTitle)
+                
                 Button("Play") {
                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                 }
+                .frame(width:  300, height: 130, alignment: .center)
+                .background(Color.yellow)
+                
+                .offset(x:0.0 ,y:250.0)
+                .font(.largeTitle)
                 
             }
                 
